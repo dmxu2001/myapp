f6b9318740ac2b6b21f44c302056b99fdd41f19e
Dongmin Xu <dmxu2001@yahoo.com>
2015-03-23 21:55:16 -0700

    add testing related code

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Profilemyrun"
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+	assert_select "title", "#{@base_title} | Home"
+
   end
 
   test "should get help" do
     get :help
     assert_response :success
+	assert_select "title", "#{@base_title} | Help"
   end
 
 end
