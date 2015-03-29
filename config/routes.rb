81e7fddb5a3829e43ee9dff7d75923f441e1a9e6
Dongmin Xu <dmxu2001@yahoo.com>
2015-03-28 20:03:33 -0700

    check in login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'index'   => 'home#index'
   get 'help'    => 'home#help'
   get 'signup'    => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
