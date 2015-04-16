eadce42adf6c6073c1fb8d7ff2161606874b4a95
Dongmin Xu <dmxu2001@yahoo.com>
2015-04-16 08:42:06 -0700

    minor google cloud access change

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,9 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for google cloud
-      :provider              => 'Google',
-      :aws_access_key_id     => ENV['GOOGLE_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['GOOGLE_SECRET_KEY']
+      :provider              			=> 'Google',
+      :google_storage_access_key_id     => ENV['GOOGLE_ACCESS_KEY'],
+      :google_storage_secret_access_key => ENV['GOOGLE_SECRET_KEY']
     }
     config.fog_directory     =  ENV['GOOGLE_BUCKET']
   end
